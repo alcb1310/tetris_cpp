a3dd819cbc639852be3620dbbe236cb82437b5b1
Andres Court <andres@andrescourt.com>
2024-11-24 09:36:35 -0500

    Added some useful game constants

--- src/constants.hpp
+++ src/constants.hpp
@@ -1,11 +1,11 @@
 #ifndef CONSTANTS_H
 #define CONSTANTS_H
 
-#define WINDOW_WIDTH 800
+#define WINDOW_WIDTH 300
 #define WINDOW_HEIGHT 600
 
 #define FPS 60
 
-#define NEARLY_BLACK Color{10, 10, 10, 255}
+#define DARK_BLUE {44, 44, 127, 255}
 
 #endif // !CONSTANTS_H
