62f359da14e19840c17c450ec952db9fd7c87119
Andres Court <andres@andrescourt.com>
2024-11-24 20:56:39 -0500

    Check for completed rows

--- src/game.cpp
+++ src/game.cpp
@@ -138,6 +138,7 @@ void Game::LockBlock() {
 
   currentBlock = nextBlock;
   nextBlock = GetRandomBlock();
+  grid.ClearFullRows();
 }
 
 bool Game::BlockFits() {
--- src/grid.hpp
+++ src/grid.hpp
@@ -12,10 +12,15 @@ public:
   void Draw();
   bool IsCellOutside(int row, int col);
   bool IsCellEmpty(int row, int col);
+  int ClearFullRows();
 
 private:
   int numRows;
   int numCols;
   int cellSize;
+
   std::vector<Color> colors;
+  bool IsRowFull(int row);
+  void ClearRow(int row);
+  void MoveRowDown(int row, int numRows);
 };
